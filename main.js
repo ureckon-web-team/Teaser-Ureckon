f562838770bc5706804fcfd96d2978e296159fd5
Sayan Das <42395405+YanDs03@users.noreply.github.com>
2018-11-09 19:07:35 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -96,6 +96,7 @@ function draw(){
     background('#240033');
      
     drawFrame();
+    drawFrameText();
     {
     // stroke('#00fff3');
     // fill('#240033');
@@ -166,17 +167,29 @@ function drawFrame(){
     stroke(255 ,0 ,0);
     noFill();
     strokeWeight(3);
-    // Outer Circle
-    line(pen.flx - 60, 160, pen.frx + 60, 160);
-    line(pen.flx - 60, 160, pen.flx - 60, 160 + pen.length + logo.h + 32 + 60);
-    line(pen.frx + 60, 160, pen.frx + 60, 160 + pen.length + logo.h + 32 + 60);
-    line(pen.flx - 60, 160 + pen.length + logo.h + 32 + 60, pen.frx + 60, 160 + pen.length + logo.h + 32 + 60);
-    // Inner Circle
-    line(pen.flx - 40, 190, pen.frx + 40, 190);
-    line(pen.flx - 40, 160 + pen.length + logo.h + 32 + 32, pen.frx + 40, 160 + pen.length + logo.h + 32 + 32);
-    line(pen.flx - 40, 190, pen.flx - 40, 160 + pen.length + logo.h + 32 + 32);
-    line(pen.frx + 40, 190, pen.frx + 40, 160 + pen.length + logo.h + 32 + 32);
+
+    // Upper Text Rect
+    rect(pen.flx-60, 195 - 30, pen.frx + 60 - (pen.flx - 60), 30, 5);
+
+    // Lower Text Rect
+    rect(pen.flx - 65, 160 + pen.length + logo.h + 32 + 32, pen.frx + 65 - (pen.flx - 65), 30, 5);
     
+    // Left side Lines
+    line(pen.flx - 25, 195, pen.flx-25, 160 + pen.length + logo.h + 32 + 32);
+    line(pen.flx - 35, 195, pen.flx-35, 160 + pen.length + logo.h + 32 + 32);
+
+    // Right side Lines
+    line(pen.frx + 25, 195, pen.frx+25, 160 + pen.length + logo.h + 32 + 32);
+    line(pen.frx + 35, 195, pen.frx+35, 160 + pen.length + logo.h + 32 + 32);
+}
+
+function drawFrameText(){
+    strokeWeight(0.9);
+    fill(14, 228, 228);
+    stroke(14, 228, 228);
+    textSize(22);
+    text("CLOCK'S TICKING", pen.flx - 40, 160 + 28);
+    text("ARE YOU READY?", pen.flx - 45, 160 + pen.length + logo.h + 32 + 32 + 24);
 }
 
 function intiPen(){
@@ -240,7 +253,7 @@ function drawPts(){
         if(points[i].y + points[i].size > height || points[i].y < 0)
             points[i].yVel *= -1;
         // Pendulum Frame Check
-        if((points[i].x + points[i].size >= pen.flx - 60 && points[i].x <= pen.frx + 60)
+        if((points[i].x + points[i].size >= pen.flx - 60 && points[i].x - points[i].size/2 <= pen.frx + 60)
             && (points[i].y > 160 && points[i].y <= 160 + pen.length + logo.h + 32 + 60)){
             points[i].yVel *= -1;
             points[i].xVel *= -1;
